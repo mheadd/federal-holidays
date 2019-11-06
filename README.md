7dffcc5e4bb3ffda9eb6759b692e88db27e671d0
Mark Headd <mheadd@gmail.com>
2019-11-06 14:57:44 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# CLI Federal Holiday List
+
+Quickly display a list of federal holidays from the command line.
+
+## Install
+
+```bash
+~$ npm install https://github.com/mheadd/federal-holidays.git
+```
+
+## Usage
+
+```bash
+~$ node path/to/index.js
+```
+
+Alternatively, you can set this up a a bash alias for convenience.
+
+```bash
+alias holidays='node path/to/index.js'
+```
---
+++ index.js
@@ -0,0 +1,28 @@
+const request = require('request');
+const HtmlTableToJson = require('html-table-to-json');
+require('console.table');
+
+// Get lists for the latest year.
+const year = process.argv[2] || 7;
+const url = `https://www.opm.gov/policy-data-oversight/pay-leave/federal-holidays/`;
+
+request(url, function (error, response, body) {
+    if (error) {
+        console.log(`Sorry. Could not retrieve holidays: ${error}`);
+    } else {
+        try {
+            // Fetch list of federal holidays.
+            const jsonTables = new HtmlTableToJson(body);
+            let holidays = jsonTables.results[year];   
+
+            // Display holiday list.
+            console.log("");
+            console.table(holidays);
+            console.log(`More information here: ${url}`);
+            console.log("");
+        }
+        catch (e) {
+            console.log(`Sorry. Could not display holidays: ${error}`);
+        }
+    }
+});
