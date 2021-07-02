873b11e678af99de41d51caf3912a247b4d08372
Mark Headd <mark.headd@gsa.gov>
2021-07-02 15:32:49 -0400

    Update for 2021 holidays

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const HtmlTableToJson = require('html-table-to-json');
 require('console.table');
 
 // Get lists for the latest year. E.g., 7th item in array is 2020.
-const year = process.argv[2] || 8;
+const year = process.argv[2] || 21;
 const url = `https://www.opm.gov/policy-data-oversight/pay-leave/federal-holidays/`;
 
 request(url, function (error, response, body) {
